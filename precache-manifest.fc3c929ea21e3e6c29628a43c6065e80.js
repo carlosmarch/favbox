1b2840afbf5fadc603166a58f340717e71a1ab56
CarlosMarch <carlosmarchinfo@gmail.com>
2020-03-24 13:31:17 +0100

    Updates

---
+++ precache-manifest.fc3c929ea21e3e6c29628a43c6065e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6cc197055c27c7debba4a44200c898",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17a90a30b725417ed846",
+    "url": "/static/css/main.f25660bd.chunk.css"
+  },
+  {
+    "revision": "aa9ff2a19f17befd5a80",
+    "url": "/static/js/2.d7532f52.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.d7532f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a90a30b725417ed846",
+    "url": "/static/js/main.e80ee500.chunk.js"
+  },
+  {
+    "revision": "1c0f7ffd2cd6b0536124",
+    "url": "/static/js/runtime-main.f579f5f4.js"
+  }
+]);
