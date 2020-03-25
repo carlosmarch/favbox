84b6206e311c97d095326983c45eb9790090a58a
CarlosMarch <carlosmarchinfo@gmail.com>
2020-03-25 09:00:03 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4303d43e3d4f0087908a3d653e285cbd.js"
+  "/precache-manifest.3026375ecde6e0d7049145c5eb87ad61.js"
 );
 
 self.addEventListener('message', (event) => {
