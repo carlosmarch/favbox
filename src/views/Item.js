b1ccf092c920df9e1ade69d7ffae2fb9720709e7
CarlosMarch <carlosmarchinfo@gmail.com>
2020-04-30 18:26:43 +0200

    Is Liked By

--- src/views/Item.js
+++ src/views/Item.js
@@ -11,7 +11,10 @@ import Like from '../components/Like';
 import Contributor from '../components/Contributor';
 import {ReactComponent as BackIcon} from '../icons/Back.svg';
 import {ReactComponent as WebIcon} from '../icons/Globe.svg';
+import {ReactComponent as LikedIcon} from '../icons/HeartFull.svg';
 
+const dataController = require('../controllers/dataController.js');
+const userController = require('../controllers/userController.js');
 
 //AIRTABLE HELPERS
 const Airtable = require('airtable');
@@ -52,7 +55,8 @@ class Item extends Component {
         this.setState({
           isLoading        : false,
           itemData         : itemData,
-          contributorData  : contributorData
+          contributorData  : contributorData,
+          isLikedBy        : this.state.itemData.isLikedBy
         });
 
       });
@@ -93,19 +97,19 @@ class Item extends Component {
                   <div className="item-content">
 
                       <div className="item-content-top">
-                        <Like itemId={this.state.itemData?.urlItemId}/>
+                        <Like itemId={this.state?.urlItemId}/>
                         <Link to={ '/categorias/' + this.state.itemData?.categorias } className="badge">{this.state.itemData?.categorias}</Link>
                         { this.state.itemData?.temas?.map((topic, key) => { return (<Link to={ '/temas/' + topic } className="badge badge-outline" key={key}>#{topic}</Link>) }) }
-
                         <h2>{this.state.itemData?.title}</h2>
-                        <p className="hero-text">{this.state.itemData?.description}</p>
                         <Contributor contributor={this.state.contributorData} className="no-m"/>
+                        <p className="hero-text">{this.state.itemData?.description}</p>
+                        {this.state.itemData?.isLikedBy ? this.getIsLikedBy() : ''}
                       </div>
 
                       <div className="item-content-bottom">
                         <a href={this.state.itemData?.url} target="_blank" rel="noopener noreferrer" className="link-box">
-                          <WebIcon className="link-box-icon icon-24" />
-                          <div className="link-box-content">
+                          <WebIcon className="link-box-icon icon-24 icon-grey" />
+                          <div className="link-box-content grey">
                             <span>Website</span>
                             <span>{Helpers.truncateText(this.state.itemData?.url, 40)}</span>
                           </div>
@@ -120,6 +124,21 @@ class Item extends Component {
     )
   }
 
+  getIsLikedBy(){
+    let isLikedArr = this.state.itemData?.isLikedBy.filter(function(person) {return person !== userController.getSession()?.name})
+    if (isLikedArr.length === 0) return
+    return (
+      <div className="is-liked-by">
+        <LikedIcon className="icon-14 icon-grey"/>
+        {isLikedArr.map((contributor, key) => {
+            return (
+              <Link to={ '/profile/' + contributor } className="grey underline-hover is-liked-name" key={key}>{contributor}</Link>
+            )
+          })
+        }liked
+      </div>
+    )
+  }
 
 
 }
